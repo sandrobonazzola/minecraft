4516cf0347b6024f785483ecb8cdfff5df789481
talshir <talia.holtzman00@gmail.com>
2017-12-24 19:06:26 +0200

    added block id values

--- minecraft.js
+++ minecraft.js
@@ -7,11 +7,14 @@ gameboard.style.width=boardWidth+"px";
 gameboard.style.height=boardHeight+"px";
 document.body.appendChild(gameboard);
 
-
+var counter=0;
 for (var i=0;i<boardHeight/50;i++){
     for(var j=0;j<boardWidth/50;j++){
+        counter++;
         var block=document.createElement("div");
         block.className="block";
+        block.id=counter;
+        console.log(counter);
         gameboard.appendChild(block);
     }
 }
