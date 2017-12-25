773ba56d285fb0651791b1422b8f5db35aa6044d
talshir <talia.holtzman00@gmail.com>
2017-12-25 16:25:18 +0200

    added sidebar/control panel

--- minecraft.js
+++ minecraft.js
@@ -1,11 +1,37 @@
 var boardWidth="800";
 var boardHeight="600";
 
-var gameboard=document.createElement("div");
+var field=document.createElement('div');
+field.style.margin="auto";
+field.style.width=parseInt(boardWidth)+100+"px";
+document.body.appendChild(field);
+
+var gameboard=document.createElement('div');
 gameboard.id="gameboard";
 gameboard.style.width=boardWidth+"px";
 gameboard.style.height=boardHeight+"px";
-document.body.appendChild(gameboard);
+field.appendChild(gameboard);
+
+var sidebar=document.createElement('div');
+sidebar.id="sidebar";
+field.appendChild(sidebar);
+
+var toolbelt=document.createElement('div');
+toolbelt.className="toolbelt";
+sidebar.appendChild(toolbelt);
+
+var toolbox=["pickaxe","axe","shovel"];
+for(var i=0;i<toolbox.length;i++){
+    var tool=document.createElement('div');
+    tool.style.backgroundImage="url(./images/"+toolbox[i]+".png)";
+    tool.className="tool";
+    tool.id=toolbox[i];
+    tool.innerHTML=toolbox[i].toUpperCase();
+    toolbelt.appendChild(tool);
+}
+var active=document.createElement('div');
+active.className="material";
+toolbelt.appendChild(active);
 
 var counter=0;
 for (var i=0;i<boardHeight/50;i++){
