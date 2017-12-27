fcf01843e2c1aec03640234d83512a7ea44753e6
TalLicht <tallicht89@gmail.com>
2017-12-27 14:19:46 +0200

    changed js script to Jquery

--- minecraft.js
+++ minecraft.js
@@ -1,4 +1,62 @@
-function setUp(){
+var ableToClick = false;
+var Minecraft = {}
+
+Minecraft.init = function(){
+    Minecraft.createBoard();
+    Minecraft.setBackGround();
+    Minecraft.play();
+}
+
+Minecraft.createBoard = function(){
+    var boardWidth = "800";
+    var boardHeight = "600";
+
+    var field = $("<div/>");
+    field.css("margin" , "auto");
+    var bWidt = parseInt(boardWidth)+100+"px";
+    field.css("width" , bWidt);
+    $("body").append(field);
+
+    var gameboard = $("<div/>");
+    gameboard.attr("id","gameboard");
+    gameboard.css("width",boardWidth+"px");
+    gameboard.css("height", boardHeight+"px");
+    field.append(gameboard);
+
+    var sidebar = $("<div/>");
+    sidebar.attr("id","sidebar");
+    field.append(sidebar);
+
+    var toolbelt = $("<div/>");
+    toolbelt.addClass("toolbelt");
+    sidebar.append(toolbelt);
+
+    var toolbox = ["pickaxe","shovel","axe"];
+    for(var i = 0 ; i < toolbox.length ; i++){
+        var tool = $("<div/>");
+        tool.css("backgroundImage", "url('./images/" + toolbox[i] +".png')");
+        tool.addClass("tool");
+        tool.attr("id",toolbox[i]);
+        tool.text(toolbox[i].toUpperCase());
+        toolbelt.append(tool);
+    }
+    var active = $("<div/>");
+    active.addClass("material");
+    toolbelt.append(active);
+
+    var counter = 0;
+    for (var i = 0 ; i < boardHeight/50 ; i++){
+        for(var j = 0 ; j < boardWidth/50; j++){
+            counter++;
+            var block = $(document.createElement("div"));
+            block.addClass("block");
+            block.attr("id",counter);
+            gameboard.append(block);
+        }
+    }
+};
+
+Minecraft.setBackGround = function(){
     for(var i=192;i>160;i--){
         $('#'+i).addClass('dirt');
     }
@@ -9,12 +67,21 @@ function setUp(){
     $('#60,#61,#62,#76,#77,#78,#92,#93,#94').addClass('leaf');
     $('#114,#115,#130,#131').addClass('rock');
     $('#35,#36,#37,#38,#34,#52,#51,#53,#21,#22,#20,#39').css('backgroundColor','white');
+};
 
+Minecraft.play = function(){    
+    //for pickaxe and rock
+    Minecraft.mine = function(){
+      // if((tool.id == "pickaxe") && (this.className == "rock")){
+      //    ableToClick = true;
+      //    if (ableToClick = true){
+      //        $(this).removeClass("rock");
+      //        $(".צaterial").addClass("rock");
+      //    }
+      // }
+      // else {
 
-}
-function minecraft(){
-    function mine(){
-        //for pickaxe and rock
+      // }
     }
     function dig(){
         //for shovel and dirt
@@ -27,50 +94,7 @@ function minecraft(){
     }
 }
 
-var boardWidth="800";
-var boardHeight="600";
 
-var field=document.createElement('div');
-field.style.margin="auto";
-field.style.width=parseInt(boardWidth)+100+"px";
-document.body.appendChild(field);
-
-var gameboard=document.createElement('div');
-gameboard.id="gameboard";
-gameboard.style.width=boardWidth+"px";
-gameboard.style.height=boardHeight+"px";
-field.appendChild(gameboard);
-
-var sidebar=document.createElement('div');
-sidebar.id="sidebar";
-field.appendChild(sidebar);
-
-var toolbelt=document.createElement('div');
-toolbelt.className="toolbelt";
-sidebar.appendChild(toolbelt);
-
-var toolbox=["pickaxe","shovel","axe"];
-for(var i=0;i<toolbox.length;i++){
-    var tool=document.createElement('div');
-    tool.style.backgroundImage="url(./images/"+toolbox[i]+".png)";
-    tool.className="tool";
-    tool.id=toolbox[i];
-    tool.innerHTML=toolbox[i].toUpperCase();
-    toolbelt.appendChild(tool);
-}
-var active=document.createElement('div');
-active.className="material";
-toolbelt.appendChild(active);
-
-var counter=0;
-for (var i=0;i<boardHeight/50;i++){
-    for(var j=0;j<boardWidth/50;j++){
-        counter++;
-        var block=document.createElement("div");
-        block.className="block";
-        block.id=counter;
-        gameboard.appendChild(block);
-    }
-}
-
-window.addEventListener("load",setUp);
+$(document).ready(function(){
+    Minecraft.init();
+});
